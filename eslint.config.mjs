057a7d294d0d211fa0b8d8b3648ed50abd644f02
Anas okasha <anasokashachama@gmail.com>
2024-12-26 01:55:30 +0200

    Add ESLint rule for exhaustive dependencies in React hooks; include additionalHooks for useMutation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react-hooks/exhaustive-deps": [
+        "warn",
+        {
+          additionalHooks: "useMutation",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
