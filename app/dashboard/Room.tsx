a473a9005f1914dbd5909b0f13cb9f0dff0e534d
Anas okasha <anasokashachama@gmail.com>
2024-12-31 21:18:02 +0200

    Refactor Room component to remove LiveblocksProvider and integrate roomId prop for improved functionality

--- app/dashboard/Room.tsx
+++ app/dashboard/Room.tsx
@@ -4,42 +4,39 @@
 
 import { EnhancedVibrantLoader } from "@/components/shared/VibrantLoader";
 import { LiveObject } from "@liveblocks/client";
-import {
-  ClientSideSuspense,
-  LiveblocksProvider,
-  RoomProvider,
-} from "@liveblocks/react/suspense";
+import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
 import { ReactNode } from "react";
 
 export function Room({
   children,
   initialPresence,
+  roomId,
 }: {
   children: ReactNode;
   initialPresence: TLiveblocks["Presence"];
+  roomId: string;
+  username: string;
 }) {
-  const publicApiKey = process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY;
+  // const publicApiKey = process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY;
 
-  if (!publicApiKey) {
-    throw new Error("Missing LIVEBLOCKS_PUBLIC_KEY");
-  }
+  // if (!publicApiKey) {
+  //   throw new Error("Missing LIVEBLOCKS_PUBLIC_KEY");
+  // }
 
   return (
-    <LiveblocksProvider publicApiKey={publicApiKey}>
-      <RoomProvider
-        initialStorage={{
-          lockedTriplets: new LiveObject({}),
-          releaseRequests: new LiveObject({}),
-          skippedTriplets: new LiveObject({}),
-          answeredRequests: new LiveObject({}),
-        }}
-        initialPresence={initialPresence}
-        id="triplet-ai-room"
-      >
-        <ClientSideSuspense fallback={<EnhancedVibrantLoader />}>
-          {children}
-        </ClientSideSuspense>
-      </RoomProvider>
-    </LiveblocksProvider>
+    <RoomProvider
+      id={roomId}
+      initialStorage={{
+        lockedTriplets: new LiveObject({}),
+        releaseRequests: new LiveObject({}),
+        skippedTriplets: new LiveObject({}),
+        answeredRequests: new LiveObject({}),
+      }}
+      initialPresence={initialPresence}
+    >
+      <ClientSideSuspense fallback={<EnhancedVibrantLoader />}>
+        {children}
+      </ClientSideSuspense>
+    </RoomProvider>
   );
 }
