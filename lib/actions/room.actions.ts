6aa313d9125a9be9504e84e0f72e25dbe8c9835b
Anas okasha <anasokashachama@gmail.com>
2024-12-31 21:18:25 +0200

    Add room actions for creating and retrieving AI rooms

---
+++ lib/actions/room.actions.ts
@@ -0,0 +1,68 @@
+"use server";
+
+import { RoomAccesses } from "@liveblocks/node";
+import { revalidatePath } from "next/cache";
+import { liveblocks } from "../liveblocks";
+import { JSONify } from "../utils";
+import { nanoid } from "nanoid";
+
+export const createASingleAI = async ({
+  userId,
+  roomTitle,
+  roomId,
+}: {
+  userId: string;
+  roomTitle: string;
+  roomId?: string;
+}) => {
+  const currentRoomId = roomId || nanoid();
+
+  const roomMetaData = {
+    creatorId: userId,
+    title: roomTitle,
+  };
+
+  const usersAccesses: RoomAccesses = {
+    [userId]: ["room:write"],
+  };
+
+  const room = await liveblocks.createRoom(currentRoomId, {
+    defaultAccesses: ["room:write"],
+    metadata: roomMetaData,
+    usersAccesses,
+  });
+
+  revalidatePath("/dashboard");
+
+  return JSONify<typeof room>(room);
+};
+
+export const createTripletAIRoom = async ({ userId }: { userId: string }) => {
+  return await createASingleAI({
+    userId,
+    roomTitle: "Triplet AI Room",
+    roomId: "triplet-ai-room",
+  });
+};
+
+export const getRoom = async ({
+  roomId,
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) => {
+  const room = await liveblocks.getRoom(roomId);
+
+  if (!room) {
+    throw new Error("Room not found");
+  }
+
+  // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+  // if (!hasAccess) {
+  //   throw new Error("You don't have access to this room");
+  // }
+
+  return JSONify<typeof room>(room);
+};
