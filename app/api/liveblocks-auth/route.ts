8b9b181c7adddd05db9acbd22638adb9a61531e1
Anas okasha <anasokashachama@gmail.com>
2024-12-31 21:17:31 +0200

    Add Liveblocks authentication route for user identification and redirection

---
+++ app/api/liveblocks-auth/route.ts
@@ -0,0 +1,21 @@
+import { getInitialPresence } from "@/lib/actions/liveblocks.actions";
+import { liveblocks } from "@/lib/liveblocks";
+import { redirect } from "next/navigation";
+
+export async function POST({}: Request) {
+  // Get the current user from your database
+  const { user } = await getInitialPresence();
+
+  if (!user) redirect("/login");
+
+  // Identify the user and return the result
+  const { status, body } = await liveblocks.identifyUser(
+    {
+      userId: user.id,
+      groupIds: [],
+    },
+    { userInfo: user }
+  );
+
+  return new Response(body, { status });
+}
