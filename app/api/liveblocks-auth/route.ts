d108786a8cf0a3a1c0b66df99f41a8d90bca6012
Anas okasha <anasokashachama@gmail.com>
2025-01-01 12:56:14 +0200

    Refactor POST handler to destructure request parameter

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -2,7 +2,7 @@ import { getInitialPresence } from "@/lib/actions/liveblocks.actions";
 import { liveblocks } from "@/lib/liveblocks";
 import { redirect } from "next/navigation";
 
-export async function POST(request: Request) {
+export async function POST({}: Request) {
   // console.log("POST /api/liveblocks-auth", request.json());
   // Get the current user from your database
   const { user } = await getInitialPresence();
